0b28df47515663cf60ec01c1dfdc59ee9cd84614
Vladimir Shatalov <valesh-soft@yandex.ru>
2026-06-18 19:28:07 +0300

    update exampe

--- examples/PressTwoButton/PressTwoButton.ino
+++ examples/PressTwoButton/PressTwoButton.ino
@@ -1,12 +1,12 @@
 /**
  * @file PressTwoButton.ino
  * @author Vladimir Shatalov (valesh-soft@yandex.ru)
- * @brief 
+ * @brief
  * @version 2.0
  * @date 22.08.2022
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  * Пример обработки одновременного нажатия на две кнопки
  */
 #include <shButton.h>
@@ -17,12 +17,12 @@
 /*
  инициализация кнопок, параметры:
 * пин, к которому подключена кнопка;
-* режим подключения кнопки: 
-	PULL_UP (значение по умолчанию) - кнопка подтянута к VCC - (pin -> кнопка -> GND); 
-	PULL_DOWN - кнопка подтянута к GND - (pin -> кнопка -> VCC;
-* тип кнопки: 
-	BTN_NO - с нормально разомкнутыми контактами (по умолчанию);
-	BTN_NC - с нормально замкнутыми контактами);
+* режим подключения кнопки:
+  PULL_UP (значение по умолчанию) - кнопка подтянута к VCC - (pin -> кнопка -> GND);
+  PULL_DOWN - кнопка подтянута к GND - (pin -> кнопка -> VCC;
+* тип кнопки:
+  BTN_NO - с нормально разомкнутыми контактами (по умолчанию);
+  BTN_NC - с нормально замкнутыми контактами);
 */
 shButton but_1(BTN1_PIN); // равнозначно shButton but_1(BTN1_PIN, PULL_UP, BTN_NO)
 shButton but_2(BTN2_PIN);
@@ -31,7 +31,9 @@ void setup()
 {
   // режим пина кнопок устанавливается автоматически
 
-  but_1.setVirtualClickOn(); // включение режима виртуального клика
+  // включение режима виртуального клика, т.к. будем отлавливать 
+  // одновременное нажатие двух кнопок по событию BTN_ONECLICK
+  but_1.setVirtualClickOn();
   but_2.setVirtualClickOn();
 
   Serial.begin(9600);
@@ -41,7 +43,8 @@ void loop()
 {
   but_1.getButtonState();
   but_2.getButtonState();
-  if (but_1.isSecondButtonPressed(but_2) || but_2.isSecondButtonPressed(but_1))
+  if (but_1.isSecondButtonPressed(but_2, BTN_ONECLICK) ||
+      but_2.isSecondButtonPressed(but_1, BTN_ONECLICK))
   { // действие по одновременно нажатым двум кнопкам
     Serial.println("Two buttons pressed");
   }
